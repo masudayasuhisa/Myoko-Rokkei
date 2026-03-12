f87fb5f21b7b658fa31b2ad621c589dbbe01192e
増田安寿 <masudayasuhisa@masudaanjunoMacBook-Air.local>
2026-03-13 02:15:18 +0900

    Microsoft Clarityのスクリプトを更新

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,16 @@ export default function RootLayout({
                         gtag('config', 'G-RV128J37EN');
                     `}
                 </Script>
+                {/* Microsoft Clarity */}
+                <Script id="microsoft-clarity" strategy="afterInteractive">
+                    {`
+                        (function(c,l,a,r,i,t,y){
+                            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                        })(window, document, "clarity", "script", "vupa6a8h9i");
+                    `}
+                </Script>
             </head>
             <body>{children}</body>
         </html>
